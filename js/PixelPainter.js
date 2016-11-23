9295c69ec321d9d5123e6d29e3ca4a8e3af8f53e
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-11-22 20:09:50 -1000

    made changes

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,153 +1,121 @@
+var pixelPainterModule = (function() {
+
+ // GRID FORM INPUTS //
+
+ var gridSizeForm = document.createElement('form');
+ gridSizeForm.id = 'gridForm';
+ pixelPainter.appendChild(gridSizeForm);
+
+ var inputBoxWidth = document.createElement('input');
+ inputBoxWidth.setAttribute('type', 'text');
+ inputBoxWidth.setAttribute('placeholder', 'Width');
+ gridSizeForm.appendChild(inputBoxWidth);
+
+ var inputBoxHeight = document.createElement('input');
+ inputBoxHeight.setAttribute('type', 'text');
+ inputBoxHeight.setAttribute('placeholder', 'Height');
+ gridSizeForm.appendChild(inputBoxHeight);
+
+ // GRID CREATE BUTTON //
+
+ var gridButton = document.createElement('div');
+ gridButton.id = 'grid-button';
+ gridButton.innerHTML = 'CREATE GRID';
+ pixelPainter.appendChild(gridButton);
+ gridButton.addEventListener('click', function() {
+   createGrid(document.getElementById("gridForm").elements[0].value, document.getElementById("gridForm").elements[1].value);
+   console.log('grid created!');
+ });
+
+ // CREATE GRID FUNCTION //
+
+ var canvas = document.createElement('div');
+ canvas.id = 'canvas';
+ pixelPainter.appendChild(canvas);
+
+ function createGrid(width, height) {
+   var table = document.createElement('table');
+   table.id = 'grid';
+   canvas.appendChild(table);
+
+   for ( var i = 0; i < height; i ++ ) {
+     var rows = document.createElement('tr');
+     grid.appendChild(rows);
+
+     for ( var j = 0; j < width; j ++ ) {
+       var columns = document.createElement('td');
+       rows.appendChild(columns);
+       columns.className = "cells";
+       columns.addEventListener('click', function (){
+          this.style.backgroundColor = selectedColor;
+        });
+     }
+   }
+ }
+
+ // COLOR PALETTE //
+
+ var selectedColor;
+
+ var colorArray = ["red", "blue", "yellow", "green",
+ "black", "orange", "purple", "pink", "brown", "aqua"];
 
 
-console.log("hi");
-
-
-window.onload = function pixelPainterModule() {
-
-
-  createGrid(10,10);
-
-  function createGrid(width, height) {
-    var table = document.createElement('table');
-    table.id = 'grid';
-    pixelPainter.appendChild(table);
-
-    for ( var i = 0; i < height; i ++ ) {
-      var rows = document.createElement('tr');
-      grid.appendChild(rows);
-       
-      
-
-     
-      for ( var j = 0; j < width; j ++ ) {
-        var columns = document.createElement('td');
-        rows.appendChild(columns);
-        rows.className = "cells";
-        columns.addEventListener('click', function (){
-           this.style.backgroundColor = selectedColor;
-         });
-         
-        
-       
-      }
-    }
-
-  }
-
-
-/*document.getElementById('grid').addEventListener('click', function(){
-    this.style.backgroundColor = selectedColor;
-});  */
-
-
-
-  var colorArray = ["red", "blue", "yellow", "green",
-  "black", "orange", "purple", "pink", "brown", "aqua"];
-
-
-  var paletteTable = document.createElement('table');
-  paletteTable.id = "palette";
-  pixelPainter.appendChild(paletteTable);
-
-
-  for(var i =0; i < colorArray.length; i++){
-    var rows = document.createElement('div');
-    rows.id = i;
-    rows.style.backgroundColor= colorArray[i];
-    rows.style.width= 20;
-    rows.style.height = 20;
-    paletteTable.appendChild(rows);
-
-  }
-
-  var selectedColor = " ";
-
-  document.getElementById(0).addEventListener('click', function (){
-    selectedColor = colorArray[0];
-    console.log(selectedColor);
-  });
-
-  document.getElementById(1).addEventListener('click', function (){
-    selectedColor = colorArray[1];
-    console.log(selectedColor);
-  });
-
-  document.getElementById(2).addEventListener('click', function (){
-    selectedColor = colorArray[2];
-    console.log(selectedColor);
-  });
-
-document.getElementById(3).addEventListener('click', function (){
-    selectedColor = colorArray[3];
-    console.log(selectedColor);
-  });
-
-document.getElementById(4).addEventListener('click', function (){
-    selectedColor = colorArray[4];
-    console.log(selectedColor);
-  });
-
-
-document.getElementById(5).addEventListener('click', function (){
-    selectedColor = colorArray[5];
-    console.log(selectedColor);
-  });
-
-document.getElementById(6).addEventListener('click', function (){
-    selectedColor = colorArray[6];
-    console.log(selectedColor);
-  });
-
-document.getElementById(7).addEventListener('click', function (){
-    selectedColor = colorArray[7];
-    console.log(selectedColor);
-  });
-
-document.getElementById(8).addEventListener('click', function (){
-    selectedColor = colorArray[8];
-    console.log(selectedColor);
-  });
-
-document.getElementById(9).addEventListener('click', function (){
-    selectedColor = colorArray[9];
-    console.log(selectedColor);
-  });
+ 
 
 
 
+ var paletteTable = document.createElement('table');
+ paletteTable.id = "palette";
+ canvas.appendChild(paletteTable);
 
+ for(var i =0; i < colorArray.length; i++){
+   var rows = document.createElement('div');
+   rows.id = i;
+   rows.style.backgroundColor= colorArray[i];
+   rows.style.width= 20;
+   rows.style.height = 20;
+   paletteTable.appendChild(rows);
+   rows.addEventListener('click', function() {
+     selectedColor = this.style.backgroundColor;
+     console.log(selectedColor);
+   });
+ }
 
+ // CLEAR BUTTON //
 
+ function clear() {
+   var tdList = document.querySelectorAll('.cells');
+   console.log(tdList.length);
+   console.log('clearing!');
+   for ( var i = 0; i < tdList.length; i ++ ) {
+     tdList[i].style.backgroundColor = 'white';
+   }
+ }
 
-  function colorPicker(color) {
-    grid.onclick= function(){
-      this.style.backgroundColor= selectedColor;
-      console.log(selectedColor);
+ var clearButton = document.createElement('div');
+ clearButton.id = 'clear-button';
+ clearButton.innerHTML = 'CLEAR';
+ pixelPainter.appendChild(clearButton);
+ clearButton.addEventListener('click', function() {
+   clear();
+ });
 
+ // ERASE BUTTON //
 
+ var eraseButton = document.createElement('div');
+ eraseButton.id = 'erase-button';
+ eraseButton.innerHTML = 'ERASE';
+ pixelPainter.appendChild(eraseButton);
+ eraseButton.addEventListener('click', function() {
+   console.log('eraser active');
+   selectedColor = 'white';
+ });
 
-    };
+ return {
+   clear: clear,
+   createGrid: createGrid
+ };
 
-
-
-  }
-
-
-    function clear() {
-  }
-
-  function erase() {
-  }
-
-  return {
-    clear: clear,
-    erase: erase,
-    colorPicker: colorPicker,
-    createGrid: createGrid
-  };
-
-};
-
-//var myPainter = pixelPainterModule();
+})();
 
